0a5c87251bb5cdee7c27f9ea5d334df74c6f0dcf
olivem <olivem@users.noreply.github.com>
2014-08-24 22:43:13 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-Enter file contents here## Put comments here that give an overall description of what your
+## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
